d09f2fd5a268f05664233063d64ced97dc34888e
Kevin Mathias <komathias1@gmail.com>
2020-05-03 07:01:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating)
+    self.where("rating = ?", self.highest_rating).flatten
   end
   
 
